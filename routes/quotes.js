09f82b3229f5952c602262fa1375582f82ce8810
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-04-07 18:49:42 +0530

    update

--- routes/quotes.js
+++ routes/quotes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Quote = require('../schema/quotes.schema');
 
 router.get('/random', async(req, res) => {
-    const quote = await Quote.aggregate([{ $sample: { size: 1 } }])
-    res.json(quote[0])
+    const quotes = await Quote.aggregate([{ $sample: { size: 1 } }])
+    const { quote, author } = quotes[0]
+    res.json({ quote, author })
 })
 
 module.exports = router
